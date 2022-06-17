4474d450ba786964c4cd82c57c94c63ae6a58896
Joey_See <mynameisjoeysee@gmail.com>
2022-06-17 17:28:08 +0800

    INITIAL SETUP ( Creating of folders and file, template for sidebar)

---
+++ application/__init_.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_cors import CORS
+from flask_talisman import Talisman
+import os
+#create the Flask app
+app = Flask(__name__)
+Bootstrap(app)
+CORS(app)
+# Wrap Flask app with Talisman
+Talisman(app, content_security_policy=None)
+
+
+from application import routes
+
---
+++ application/routes.py
@@ -0,0 +1,21 @@
+import email
+from webbrowser import get
+from application import app, db
+from application.models import User,ImageTable
+from datetime import datetime
+from application.forms import  LoginForm, RegisterForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from flask_login import LoginManager,  login_user, login_required, logout_user, current_user
+from flask import render_template, request, flash, json, jsonify,redirect,url_for 
+from flask_cors import CORS, cross_origin
+import pathlib, os
+import requests
+import keras.models
+from PIL import Image, ImageOps
+from keras.preprocessing import image
+import numpy as np
+import re
+import base64
+import tempfile
+import numpy as np

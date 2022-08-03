6c724c33bdde52cd5f0f4d8e2ee9ca986a6464e4
Joey-see <94065073+Joey-see@users.noreply.github.com>
2022-08-03 20:35:34 +0800

    Revert "Login Functions Done"

--- application/forms.py
+++ application/forms.py
@@ -4,14 +4,14 @@ from wtforms.validators import InputRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)],render_kw={"placeholder":"Username"})
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)],render_kw={"placeholder":"Password"})
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
     
 
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)],render_kw={"placeholder":"Email"})
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)],render_kw={"placeholder":"Username"})
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)],render_kw={"placeholder":"Password"})
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
 
 
 
